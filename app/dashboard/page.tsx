25c11f9f1757de90108a9116351c97338b31ca27
Elihu Nibra <nibraelihu@gmail.com>
2024-09-06 00:22:24 -0600

    First steps and configuration on the project

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+export default function Dashboard() {
+    return (
+        <div>This is the dashboard page, here you will find the main part of the weather dashboard</div>
+    );
+}
---
+++ app/not-found.tsx
@@ -0,0 +1,5 @@
+export default function Error() {
+    return ( <div>
+        <h1>Oops! Something went wrong</h1>
+    </div>);
+}
